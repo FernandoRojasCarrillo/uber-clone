62ab635b5865681994f3e4c4ccacf92dd545b260
FernandoRojasCarrillo <diego27fernando72@gmail.com>
2024-09-03 16:50:41 -0500

    Initial commit

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,12 @@
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity></TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
