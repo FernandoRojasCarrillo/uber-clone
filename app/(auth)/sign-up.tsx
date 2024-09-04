63cb8b0519fa2e5d3a31a659ca400b4acadf6ac3
FernandoRojasCarrillo <diego27fernando72@gmail.com>
2024-09-04 18:12:56 -0500

    Login in progress

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,142 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [verification, setVerification] = useState({
+    state: "default",
+    error: "",
+    code: "",
+  });
+
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      setVerification((prev) => ({
+        ...prev,
+        state: "pending",
+      }));
+    } catch (err: any) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) return;
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code,
+      });
+
+      if (completeSignUp.status === "complete") {
+        // TODO: Create a database user!
+
+        await setActive({ session: completeSignUp.createdSessionId });
+        setVerification((prev) => ({ ...prev, state: "success" }));
+      } else {
+        setVerification((prev) => ({
+          ...prev,
+          error: "Varification failed",
+          state: "failed",
+        }));
+      }
+    } catch (err: any) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      setVerification((prev) => ({
+        ...prev,
+        error: err.error[0].longMessage || "Error",
+        state: "failed",
+      }));
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Text>SignUp</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+
+        <View className="p-5 ">
+          <InputField
+            label="Name"
+            placeholder="Enter you name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) =>
+              setForm((prevValue) => ({ ...prevValue, name: value }))
+            }
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter you email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) =>
+              setForm((prevValue) => ({ ...prevValue, email: value }))
+            }
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter you password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) =>
+              setForm((prevValue) => ({ ...prevValue, password: value }))
+            }
+          />
+
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
+
+        {/* Verification Modal */}
+      </View>
+    </ScrollView>
   );
 };
 
